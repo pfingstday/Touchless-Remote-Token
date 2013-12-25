ec03ea352cfd74386d104fe629e47cdc257fd358
Philipp Pfingsttag <pp@zeitmacht.com>
2013-12-25 04:19:14 +0100

    Added swipe detection, changed RFID Lib

--- Touchless_Remote_Token.ino
+++ Touchless_Remote_Token.ino
@@ -1,7 +1,7 @@
 
 // RFID Lib
 #include <SPI.h>
-#include <RFID.h>
+#include <MFRC522.h>
 
 // RCSwitch Lib
 #include <RCSwitch.h>
@@ -9,9 +9,9 @@
 // IRremote Lib
 #include <IRremote.h>
 
-
-#define irPin1 0 // analog Sharp IR 1
-#define irPin2 1 // analog Sharp IR 1
+// Swipe Detector
+#include "SwipeDetector.h"
+#include "ProximitySensor.h"
 
 #define rcPin 2 // RCSwitch
 
@@ -20,195 +20,197 @@
 
 // Default IR Led Pin 3 (Uno), Pin 13 (Micro)
 
+SharpIRProximitySensor sensorLeft(0);
+SharpIRProximitySensor sensorRight(1);
+SwipeDetector detector;
 
-// RFID Setup
-int serNum0;
-int serNum1;
-int serNum2;
-int serNum3;
-int serNum4;
-
-RFID rfid(SS_PIN, RST_PIN); 
+//RFID Setup
+MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 // IRemote Setup
 IRsend irsend;
 
-// RCSwitch Lib
-RCSwitch mySwitch = RCSwitch();    
+// RCSwitch Setup
+RCSwitch mySwitch = RCSwitch(); 
 
 
 void setup(){
-  
- /* Sharp IR */
+
+  /* Sharp IR */
   Serial.begin(9600);
   SPI.begin(); 
-  rfid.init();
 
- /* RCSwitch */
+  /* Init MFRC522 card */
+  mfrc522.PCD_Init();
+
+  /* RCSwitch */
   mySwitch.enableTransmit(rcPin); 
 }
 
 
 void loop(){
-  
- /* Sharp IR */
-  
-  float volts = analogRead(irPin1)*0.0048828125;   // value from sensor * (5/1024) - if running 3.3.volts then change 5 to 3.3
-  int distance1 = 12*pow(volts, -1);               // worked out from graph
-  distance1 = constrain (distance1, 0, 30);
-  Serial.println(distance1);                       
-  delay(100);   
 
+  /* Sharp IR */
 
- /* RFID RC522 */
+  int distance = sensorLeft.read();
+  int distance2 = sensorRight.read();
 
-  if (rfid.isCard()) {
-    if (rfid.readCardSerial()) {
-      if (rfid.serNum[0] != serNum0
-        && rfid.serNum[1] != serNum1
-        && rfid.serNum[2] != serNum2
-        && rfid.serNum[3] != serNum3
-        && rfid.serNum[4] != serNum4
-        ) {
+  SwipeDetector::Swipe s = detector.detect(distance, distance2);
 
-        serNum0 = rfid.serNum[0];
-        serNum1 = rfid.serNum[1];
-        serNum2 = rfid.serNum[2];
-        serNum3 = rfid.serNum[3];
-        serNum4 = rfid.serNum[4];
-
-        Serial.print(rfid.serNum[0],HEX);
-        Serial.print(rfid.serNum[1],HEX);
-        Serial.print(rfid.serNum[2],HEX);
-        Serial.print(rfid.serNum[3],HEX);
-        Serial.print(rfid.serNum[4],HEX);
-        
-        } else {
-        // Same ID = Do nothing
-        Serial.print(".");
-      }
-    } 
-
-
-/* RCSwitch */
-
-if (rfid.serNum[1] == 0x90) {
-  
-  Serial.println("Switch");
-
-        
-  if (distance1 <= 5) {  
-
-    Serial.println("Switch Off");  
-
-    mySwitch.switchOff("00000", "10000");
-    delay(10);
-
-    mySwitch.switchOff("00000", "01000");
-    delay(10);
-
-    mySwitch.switchOff("00000", "00100");
-    delay(10);
-
-    mySwitch.switchOff("11111", "10000");
-    delay(10);
-
-    mySwitch.switchOff("11111", "01000");
-    delay(10);
-
-    mySwitch.switchOff("11111", "00100");
-    delay(10);
+  if (distance != -1 && distance2 != -1) {  // -1 = kein gültiges Signal
+    Serial.println(distance);
+    Serial.println(distance2);
   }
 
 
-  if (distance1 > 10 && distance1 < 20) {
+  /* Swipe Detection */
 
-    Serial.println("Switch On");  
+  if (mfrc522.uid.uidByte[1] == 0x6F) {
 
-    mySwitch.switchOn("00000", "10000");
-    delay(10);
+    //Serial.println("Swipe Detection");
 
-    mySwitch.switchOn("00000", "01000");
-    delay(10);
+    if (s == SwipeDetector::SWIPE_LEFT) {
+      Serial.println("Swipe: Previous");
+      Remote.rewind();
+      Remote.clear();  
+    }
 
-    mySwitch.switchOn("00000", "00100");
-    delay(10);
+    if (s == SwipeDetector::SWIPE_RIGHT) {
+      Serial.println("Swipe: Next");
+      Remote.forward();
+      Remote.clear(); 
+    }
+  }  
 
-    mySwitch.switchOn("11111", "10000");
-    delay(10);
 
-    mySwitch.switchOn("11111", "01000");
-    delay(10);
+  /* RFID RC522 */
 
-    mySwitch.switchOn("11111", "00100");
-    delay(10);
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
   }
-}
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+  // Now a card is selected. The UID and SAK is in mfrc522.uid.
 
 
- /* Keyboard Maestro */
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+  //	Serial.print(mfrc522.uid.uidByte[i], HEX);
+  } 
+  //	Serial.println();
 
-  if (rfid.serNum[1] == 0xF9) {
-    
-    Serial.println("Keyboard");
 
+  /* RCSwitch */
 
-    if (distance1 > 15 && distance1 < 25){
-      Serial.println("Mac Lauter");
-      Keyboard.press(KEY_F12);
-      Keyboard.release(KEY_F12);
+  if (mfrc522.uid.uidByte[1] == 0x90 && distance !=-1) {
+
+    Serial.println("Switch");
+
+    if (distance <= 5) {  
+
+      Serial.println("Switch Off");  
+
+      mySwitch.switchOff("00000", "10000");
+      delay(10);
+
+      mySwitch.switchOff("00000", "01000");
+      delay(10);
+
+      mySwitch.switchOff("00000", "00100");
+      delay(10);
+
+      mySwitch.switchOff("11111", "10000");
+      delay(10);
+
+      mySwitch.switchOff("11111", "01000");
+      delay(10);
+
+      mySwitch.switchOff("11111", "00100");
       delay(10);
     }
-    
-    if (distance1 <= 10) {
-      Serial.println("Mac Leiser");
-      Keyboard.press(KEY_F11);
-      Keyboard.release(KEY_F11);
+
+
+    if (distance > 10 && distance < 20) {
+
+      Serial.println("Switch On");  
+
+      mySwitch.switchOn("00000", "10000");
+      delay(10);
+
+      mySwitch.switchOn("00000", "01000");
+      delay(10);
+
+      mySwitch.switchOn("00000", "00100");
+      delay(10);
+
+      mySwitch.switchOn("11111", "10000");
+      delay(10);
+
+      mySwitch.switchOn("11111", "01000");
+      delay(10);
+
+      mySwitch.switchOn("11111", "00100");
       delay(10);
     }
-    
-    if (distance1 <= 5) {
+  }
+
+
+  /* USB HID */
+
+  if (mfrc522.uid.uidByte[1] == 0x19 && distance !=-1) { 
+
+    // Serial.println("USB HID");
+
+    if (distance > 15 && distance < 25) {
+      Serial.println("Volume Up");
+      Remote.increase();
+      Remote.clear();
+      delay(100);
+    }
+
+    if (distance > 5 && distance < 10) {
+      Serial.println("Volume Down");
+      Remote.decrease();
+      Remote.clear();
+      delay(100);
+    }
+
+    if (distance <= 5) {
       Serial.println("Mute");
-      Keyboard.press(KEY_F10);
-      Keyboard.release(KEY_F10);
+      Remote.mute();
+      Remote.clear();
       delay(1000);
-    }  
+    }
   }
 
 
-/* IRemote */
+  /* IRemote */
 
-  if (rfid.serNum[1] == 0x45) {
-    
-    Serial.println("IR Receiver");
+  if (mfrc522.uid.uidByte[1] == 0x45 && distance != -1) {
 
-   
-    if(distance1 > 15 && distance1 < 25) {
-    Serial.println(" IR Lauter");
-    irsend.sendNEC(0xA55A50AF, 32);
-    delay(40);
+    //    Serial.println("IR Receiver");
+
+    if (distance > 15 && distance < 25) {
+      Serial.println(" IR Lauter");
+      irsend.sendNEC(0xA55A50AF, 32);
+      delay(40);
     }
 
-    if (distance1 <= 10) {
+    if (distance <= 10) {
       Serial.println("IR Leiser");
       irsend.sendNEC(0xA55AD02F, 32);
       delay(40);
     }
 
-    if (distance1 <= 5) {
+    if (distance <= 5) {
       Serial.println("IR Mute");
       irsend.sendNEC(0xA55A48B7, 32);
       delay(1000);
     }
   }
-
-
-
-  }
-
-  rfid.halt(); 
-
 }
-
 
 
