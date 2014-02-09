9d2d6fa0db9aa41ea69875f54d4d3fec8c4a02ef
Philipp Pfingsttag <pp@zeitmacht.com>
2014-02-09 14:53:38 +0100

    Added fading for volume feedback

--- Touchless_Remote_Token.ino
+++ Touchless_Remote_Token.ino
@@ -15,7 +15,6 @@
 #include "SwipeDetector.h"
 #include "ProximitySensor.h"
 
-
 // Default IR Led 
 // Pin 3 (Uno), Pin 13 (Micro)
 
@@ -24,8 +23,28 @@ const int redPin = 12;
 const int greenPin = 11;
 const int bluePin = 10;
 
-int brightness = 0;    // how bright the LED is
-int fadeAmount = 5;    // how many points to fade the LED by
+// Swipe & HID
+int i1 = 255; 
+int redVal1 = 0;
+int greenVal1 = 255;
+int blueVal1 = 255;
+
+int i2 = 125; 
+int redVal2 = 255;
+int greenVal2 = 0;
+int blueVal2 = 0;
+
+//int redVal3 = 0;
+//int greenVal3 = 120;
+//int blueVal3 = 120;
+//
+//int redVal4 = 0;
+//int greenVal4 = 120;
+//int blueVal4 = 120;
+//
+//int redVal5 = 0;
+//int greenVal5 = 120;
+//int blueVal5 = 120;
 
 // RFID
 const int RST_PIN = 8;  
@@ -51,16 +70,21 @@ SwipeDetector detector;
 // RFID Tags Setup
 byte tag;
 
-//Tilt
-//int tilt_s1 = 3;
-//int tilt_s2 = 4;
+// Delay Setup
+boolean delayOn = true;
+Metro delaySensor = Metro(1000);
+
+boolean resetColor = true;
+Metro delayColor = Metro(2000);
 
 boolean MusicOff = false;
 boolean SwitchOff = false;
 boolean MuteOn = false;
 
-boolean delayOn = true;
-Metro delaySensor = Metro(1000);
+
+//Tilt
+//int tilt_s1 = 3;
+//int tilt_s2 = 4;
 
 
 void setup(){
@@ -80,9 +104,10 @@ void setup(){
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
-  
-// pinMode(tilt_s1, INPUT);
-// pinMode(tilt_s2, INPUT);
+
+  /* Tilt */
+  // pinMode(tilt_s1, INPUT);
+  // pinMode(tilt_s2, INPUT);
 }
 
 
@@ -95,30 +120,28 @@ void readrfid(){
   if ( ! mfrc522.PICC_ReadCardSerial()) {
     return;
   }
-  // Now a card is selected. The UID and SAK is in mfrc522.uid.
 
   for (byte i = 0; i < mfrc522.uid.size; i++) {
-  //  Serial.print(mfrc522.uid.uidByte[i], HEX);
+    // Serial.print(mfrc522.uid.uidByte[i], HEX);
   } 
-  //  Serial.println();
+    // Serial.println();
 }
 
 
 void loop(){
   
-//  int position = getTiltPosition();
-//  Serial.println(position);
+  //  int position = getTiltPosition();
+  //  Serial.println(position);
   
 
   /* RFID RC522 */
   
   readrfid();
 
-  if(mfrc522.uid.uidByte[1] == 0x6F){tag = 1;} // Swipe
-  if(mfrc522.uid.uidByte[1] == 0x19){tag = 2;} // HID
-  //if(mfrc522.uid.uidByte[1] == 0x45){tag = 3;} // IRemote
-  if(mfrc522.uid.uidByte[0] == 0xDA){tag = 3;} // IRemote
-  if(mfrc522.uid.uidByte[1] == 0x90){tag = 4;} // RCSwitch
+  if(mfrc522.uid.uidByte[1] == 0x6F){tag = 1;} // Swipe HID LILA
+  if(mfrc522.uid.uidByte[1] == 0x19){tag = 2;} // Remote HID BLAU
+  //if(mfrc522.uid.uidByte[1] == 0x45){tag = 3;} // -
+  if(mfrc522.uid.uidByte[1] == 0x90){tag = 4;} // Switch GELB
   if(mfrc522.uid.uidByte[1] == 0x79){tag = 5;} // MasterSwitch
   
   
@@ -129,10 +152,10 @@ void loop(){
 
   SwipeDetector::Swipe s = detector.detect(distance, distance2);
   
-//  if (distance != -1 && distance2 != -1) {
-//  Serial.println(distance);
-//  Serial.println(distance2);
-//   }
+  //if (distance != -1 && distance2 != -1) {
+  //Serial.println(distance);
+  //Serial.println(distance2);
+  //}
    
    
  /* Sensor Delay */  
@@ -141,281 +164,288 @@ void loop(){
      
    if (delayOn == true && distance != -1 && distance < 28 && distance2 < 28){
     delayOn = false;
-     Serial.println("delayOn = false");
+     //Serial.println("delayOn = false");
     }else{
      delayOn = true;
-     Serial.println("delayOn = true");
+     //Serial.println("delayOn = true");
+    }
+ }
+ 
+ /* Color Reset */  
+   
+   if (delayColor.check() == 1) {
+     
+   if (resetColor == true && distance != -1 && distance < 28 && distance2 < 28){
+    resetColor = false;
+     Serial.println("resetColor = false");
+    }else{
+     resetColor = true;
+     Serial.println("resetColor = true");
     }
  }
 
-//if (distance2 >= 4 && distance2 <= 25){
-//   analogWrite(bluePin, brightness); 
-//  analogWrite(greenPin, brightness);  
-//
-//  // change the brightness for next time through the loop:
-//  brightness = brightness + fadeAmount;
-//
-//  // reverse the direction of the fading at the ends of the fade: 
-//  if (brightness == 0 || brightness == 255) {
-//    fadeAmount = -fadeAmount ; 
-//  }     
-//  // wait for 30 milliseconds to see the dimming effect    
-//  delay(30);        
-//}else{
-//   setColor(0, 0, 0);
-//}
+
+ /* Tag 1: HID Volume & Swipe  */ 
   
+//  if (MusicOff == false && tag == 1) {
+//     setColor(255, 100, 100);    
+//  }
+//
+//  else if (MusicOff == true && tag == 1) {
+//    setColor(0, 0, 0);
+//  }
 
-/* Tag 1: Swipe & HID Volume */ 
-  
-    if (MusicOff == false && tag == 1) {
-     setColor(255, 100, 100);    
-  }
+  if (tag == 1 && resetColor == true) {
+    setColorTag1(redVal1,greenVal1,blueVal1);
+  }    
 
-  else if (MusicOff == true && tag == 1) {
-    setColor(0, 0, 0);
-  }
+ // Skip Tracks
 
+ if (tag == 1) {
 
- /* Swipe Detection */
+    if (delayOn == true && MusicOff == false && MuteOn == false) {
+      
+     if (s == SwipeDetector::SWIPE_LEFT) {
+        Serial.println("Swipe: Previous");
+        Remote.rewind();
+        Remote.clear();
+        setColor(255, 0, 0);
+        delay(300);
+        setColorTag1(redVal1,greenVal1,blueVal1);
+        delaySensor.reset();
+      }
 
-  if (tag == 1 && delayOn == true && MusicOff == false && MuteOn == false) {
-    
-   if (s == SwipeDetector::SWIPE_LEFT) {
-      Serial.println("Swipe: Previous");
-      Remote.rewind();
-      Remote.clear();
-      setColor(255, 0, 0);
-      delay(300);
+      if (s == SwipeDetector::SWIPE_RIGHT) {
+        Serial.println("Swipe: Next");
+        Remote.forward();
+        Remote.clear();
+        setColor(0, 255, 0);
+        delay(300);
+        setColorTag1(redVal1,greenVal1,blueVal1);
+      }
     }
-
-    if (s == SwipeDetector::SWIPE_RIGHT) {
-      Serial.println("Swipe: Next");
-      Remote.forward();
-      Remote.clear();
-      setColor(0, 255, 0);
-      delay(300);
-    }
-  }
-    
- 
-/* HID Volume Control */  
-   
-  if(delayOn == false){   
+       
+    // Volume Control   
      
-   if (distance2 != -1 && distance2 >= 12 && distance2 <= 25) {
-      Serial.println("Volume Up");
-      Remote.increase();
-      Remote.clear();
-      sensorRight.clear();
-      delaySensor.reset();
-      //setColor(255, 100, 100);
-      delay(200);
-      
-      MusicOff = false;
+    if (delayOn == false && resetColor == false) {   
+       
+     if (distance2 != -1 && distance2 >= 12 && distance2 <= 25) {
+        Serial.println("Volume Up");
+        Remote.increase();
+        Remote.clear();
+        sensorRight.clear();
+        delaySensor.reset();
+        setColorTag1(redVal1,greenVal1,blueVal1);
+    
+        i1 += 16;
+        redVal1  = 0;      
+        greenVal1 += 16; 
+        blueVal1  += 16;
+    
+        if (i1 >= 255){
+          redVal1  = 0;
+          greenVal1 = 255;
+          blueVal1  = 255;
+          i1 = 255;
+        }
+
+        delayColor.reset();
+        delay(150);
+        
+        MusicOff = false;
+      }
+
+      if (distance2 != -1 && distance2 >= 5 && distance2 <= 8) {
+        Serial.println("Volume Down");
+        Remote.decrease();
+        Remote.clear();
+        sensorRight.clear();
+        delaySensor.reset();         
+        setColorTag1(redVal1,greenVal1,blueVal1);
+    
+        i1 -= 16;
+        redVal1  = 0;
+        greenVal1 -= 16; 
+        blueVal1  -= 16;
+    
+        if (i1 <= 16){
+          redVal1  = 0;
+          greenVal1 = 16;
+          blueVal1  = 16;
+          i1 = 16;
+        }
+
+        delayColor.reset();
+        delay(150);
+        
+        MusicOff = false;
+      }  
     }
 
-    if (distance2 != -1 && distance2 >= 5 && distance2 <= 8) {
-      Serial.println("Volume Down");
-      Remote.decrease();
-      Remote.clear();
-      sensorRight.clear();
-      delaySensor.reset();
-      //setColor(255, 100, 100);
-      delay(200);
-      
-      MusicOff = false;
-    }
+    // Mute & Play     
    
-   }
-   
-   
-if (tag == 1 && MuteOn == true && delayOn == true) {
-    
-    if (s == SwipeDetector::SWIPE_LEFT || s == SwipeDetector::SWIPE_RIGHT) {
-      Serial.println("Play");
-      Remote.mute();
-      Remote.clear();
-      sensorRight.clear();
-      //delaySensor.reset();
-      //delayOn = false;
-      //MusicOff = false;
-      MuteOn = false;
-    }
-  }
-  
-  if (distance2 != -1 && distance2 <= 4) {
+    if (distance2 != -1 && distance2 <= 4) {
       Serial.println("Mute");
       Remote.mute();
       Remote.clear();
       sensorRight.clear();
       //setColor(0, 0, 0);
-      //MusicOff = true;
+      MusicOff = true;
       MuteOn = true;
       delay(900);
-      delaySensor.reset();
-      
-      
-      
+      delaySensor.reset();  
     }
 
-
-//  /* USB HID */
-//  
-//  if (MusicOff == false && tag == 2) {
-//    setColor(100, 100, 100); 
-//  }
-//
-//  else if (MusicOff == true && tag == 2) {
-//    setColor(0, 0, 0);
-//  }
-//    
-//
-//  if (tag == 2 && distance != -1) { 
-//    
-//    Serial.println("USB HID");
-//
-//    if (distance >= 10 && distance <= 20) {
-//      Serial.println("Volume Up");
-//      Remote.increase();
-//      Remote.clear();
-//      setColor(255, 255, 255);
-//      delay(100);
-//      
-//      MusicOff = false;
-//    }
-//
-//    if (distance >= 5 && distance <= 8) {
-//      Serial.println("Volume Down");
-//      Remote.decrease();
-//      Remote.clear();
-//      setColor(10, 10, 10);
-//      delay(100);
-//      
-//      MusicOff = false;
-//    }
-//
-//    if (distance <= 4) {
-//      Serial.println("Mute");
-//      Remote.mute();
-//      Remote.clear();
-//      setColor(0, 0, 0);
-//      sensorLeft.clear();
-//      delay(1000);
-//      
-//      MusicOff = true;
-//    }
-//  }
+    if (MuteOn == true && delayOn == true) {
+      
+      if (s == SwipeDetector::SWIPE_LEFT || s == SwipeDetector::SWIPE_RIGHT) {
+        Serial.println("Play");
+        Remote.mute();
+        Remote.clear();
+        sensorRight.clear();
+        //setColor(0, 0, 0);
+        MusicOff = false;
+        MuteOn = false;
+      }
+    }
+  }
 
 
-  /* IRemote */
+
+  /* Tag 2: IRRemote & Swipe */
   
-  if (tag == 3) {
-    setColor(150, 40, 20);
-  }
+  if (tag == 2 && resetColor == true) {
+    setColorTag2(redVal2,greenVal2,blueVal2);
+  }  
 
-  if (tag == 3 && distance != -1) {
+  if (tag == 2 && resetColor == false) {
 
-    //Serial.println("IR Receiver");
+   //Serial.println("IR Receiver");
 
-    if (distance >= 10 && distance <= 20) {
-      Serial.println("IR Lauter");
-      irsend.sendNEC(0xA55A50AF, 32);
-      delay(40);
+   if (distance2 >= 8 && distance2 <= 25) {
+    //Serial.println("IR Lauter");
+    irsend.sendNEC(0xA55A50AF, 32);
+
+    setColorTag2(redVal2,greenVal2,blueVal2);
+
+    i2 += 10;      
+    greenVal2 += 10; 
+    blueVal2  += 10;
+
+    if (i2 >= 255){
+      greenVal2 = 255;
+      blueVal2  = 255;
+      i2 = 255;
     }
+    delay(100);
+    delayColor.reset();
+   }
 
-    if (distance >= 5 && distance <= 8) {
-      Serial.println("IR Leiser");
-      irsend.sendNEC(0xA55AD02F, 32);
-      delay(40);
+
+   if (distance2 >= 5 && distance2 <= 8) {
+    //Serial.println("IR Leiser");
+    irsend.sendNEC(0xA55AD02F, 32);
+    
+    setColorTag2(redVal2,greenVal2,blueVal2); 
+
+    i2 -= 10;
+    greenVal2 -= 10; 
+    blueVal2  -= 10;
+
+    if (i2 <= 10){
+      greenVal2 = 10;
+      blueVal2  = 10;
+      i2 = 10;
     }
+    delay(100);
+    delayColor.reset();
+   }
+}
 
-    if (distance <= 4) {
-      Serial.println("IR Mute");
-      irsend.sendNEC(0xA55A48B7, 32);
-      sensorLeft.clear();
-      delay(1000);
-    }
-  }
+   if (distance2 <= 4 && distance2 != -1) {
+     Serial.println("IR Mute");
+     irsend.sendNEC(0xA55A48B7, 32);
+     sensorLeft.clear();
+     delay(1000);
+   }
+ 
+  
 
-
-  /* RCSwitch */
+  /* Tag 4: RCSwitch */
 
   if (SwitchOff == false && tag == 4) {
-    setColor(230, 50, 0);
+    //setColor(200, 50, 0);
   }
     
   else if (SwitchOff == true && tag == 4) {
-    setColor(0, 0, 0); // Lights off!
+    //setColor(0, 0, 0); // Lights off!
   } 
 
   if (tag == 4) {
     
-    Serial.println("Switch");
+    //Serial.println("Switch");
 
     if (s == SwipeDetector::SWIPE_LEFT || s == SwipeDetector::SWIPE_RIGHT) {
 
       Serial.println("Switch On");  
-      setColor(0, 255, 0);
+      //setColor(0, 255, 0);
       
-        mySwitch.switchOn("00000", "10000");
-        delay(10);
-        mySwitch.switchOn("00000", "01000");
-        delay(10);
-        mySwitch.switchOn("00000", "00100");
-        delay(10);      
+      mySwitch.switchOn("00000", "10000");
+      delay(10);
+      mySwitch.switchOn("00000", "01000");
+      delay(10);
+      mySwitch.switchOn("00000", "00100");
+      delay(10);      
+      //mySwitch.switchOn("11111", "10000");
+      //delay(10);
+      //mySwitch.switchOn("11111", "01000");
+      //delay(10);
+      //mySwitch.switchOn("11111", "00100");
 
-//      mySwitch.switchOn("11111", "10000");
-//      delay(10);
-//      mySwitch.switchOn("11111", "01000");
-//      delay(10);
-//      mySwitch.switchOn("11111", "00100");
-//      delay(10);
-      
       SwitchOff = false;
     }
  
     if (distance >0 && distance <= 4) {  
 
       Serial.println("Switch Off");  
-      setColor(255, 0, 0);
+      //setColor(255, 0, 0);
       
-        mySwitch.switchOff("00000", "10000");
-        delay(10);
-        mySwitch.switchOff("00000", "01000");
-        delay(10);
-        mySwitch.switchOff("00000", "00100");
-        delay(10);
-//      mySwitch.switchOff("11111", "10000");
-//      delay(10);
-//      mySwitch.switchOff("11111", "01000");
-//      delay(10);
-//      mySwitch.switchOff("11111", "00100");
-//      delay(10);
-      
+      mySwitch.switchOff("00000", "10000");
+      delay(10);
+      mySwitch.switchOff("00000", "01000");
+      delay(10);
+      mySwitch.switchOff("00000", "00100");
+      delay(10);
+      //mySwitch.switchOff("11111", "10000");
+      //delay(10);
+      //mySwitch.switchOff("11111", "01000");
+      //delay(10);
+      //mySwitch.switchOff("11111", "00100");
+      //delay(10);
+
       SwitchOff = true;
     }
   }
 
 
-  /* MasterSwitch */
+  /* Tag 5: MasterSwitch */
 
   if (SwitchOff == false && tag == 5) {
-    setColor(0, 0, 255);
+    //setColor(255, 255, 20);
   }
   
   else if (SwitchOff == true && tag == 5) {
-    setColor(0, 0, 0); // Lights off!
+    //setColor(0, 0, 0); // Lights off!
   } 
 
   if (tag == 5) {
     
-    Serial.println("Master Switch");
+    //Serial.println("Master Switch");
 
     if (s == SwipeDetector::SWIPE_LEFT || s == SwipeDetector::SWIPE_RIGHT) {
 
       Serial.println("Switch On");  
-      setColor(0, 255, 0);
+      //setColor(0, 255, 0);
 
       mySwitch.switchOn("00000", "10000");
       delay(10);
@@ -436,7 +466,7 @@ if (tag == 1 && MuteOn == true && delayOn == true) {
     if (distance >0 && distance <= 4) {  
 
       Serial.println("Switch Off");  
-      setColor(255, 0, 0);
+      //setColor(255, 0, 0);
 
       mySwitch.switchOff("00000", "10000");
       delay(10);
@@ -461,6 +491,18 @@ void setColor(int red, int green, int blue) {
   analogWrite(redPin, red);
   analogWrite(greenPin, green);
   analogWrite(bluePin, blue); 
+}
+
+void setColorTag1(int redVal1, int greenVal1, int blueVal1) {
+  analogWrite(redPin, redVal1);
+  analogWrite(greenPin, greenVal1);
+  analogWrite(bluePin, blueVal1); 
+}
+
+void setColorTag2(int redVal2, int greenVal2, int blueVal2) {
+  analogWrite(redPin, redVal2);
+  analogWrite(greenPin, greenVal2);
+  analogWrite(bluePin, blueVal2); 
 }
 
 //int getTiltPosition(){
